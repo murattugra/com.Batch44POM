f4751f7be69d9032b642aae575706ba0afafb7d8
Murat Arslan <mrterkmglsv@gmail.com>
2022-02-04 20:29:00 +0100

    asas

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -10,8 +10,6 @@ public class ConfigReader {
     public static Properties properties;
 
     static{
-
-
         String path="src/configuration.properties";
         try {
             FileInputStream fis=new FileInputStream(path);
